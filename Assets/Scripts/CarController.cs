6201c5b38bdd74cd81e0f0065e75c7a60e5db623
Ender Erol <endererol.work@gmail.com>
2023-11-17 12:59:11 +0300

    Drift and Turn Logic Added

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -17,133 +17,222 @@ namespace Race
         public WheelFrictionCurve originalForwardFriction;
         public WheelFrictionCurve originalSidewaysFriction;
     }
-    
+
     public class CarController : MonoBehaviour
     {
-        [Header("Axle Information")] 
-        [SerializeField] AxleInfo[] axleInfos;
+        [Header("Axle Information")] [SerializeField]
+        AxleInfo[] axleInfos;
 
-        [Header("Motor Attributes")] 
-        [SerializeField]  float maxMotorTorque = 3000f;
-        [SerializeField]  float maxSpeed;
+        [Header("Motor Attributes")] [SerializeField]
+        float maxMotorTorque = 3000f;
 
-        [Header("Steering Attributes")] 
-        [SerializeField]  float maxSteeringAngle = 30f;
+        [SerializeField] float maxSpeed;
 
-        [Header("Braking and Drifting Attributes")] 
-        [SerializeField]  float brakeTorque = 10000f;
-        
-        private float brakeVelocity;
-        
+        [Header("Steering Attributes")] [SerializeField]
+        float maxSteeringAngle = 30f;
 
-        [SerializeField]  InputReader input;
+        [SerializeField] private AnimationCurve turnCurve;
+        [SerializeField] private float turnStrength = 1500f;
+
+        [Header("Braking and Drifting Attributes")] [SerializeField]
+        private float driftSteeringMultiplier = 1.5f; //change steering when drifting
+
+        [SerializeField] float brakeTorque = 10000f;
+
+        [Header("Physics")] [SerializeField] private Transform centerOfMass;
+        [SerializeField] private float downForce = 100f;
+        [SerializeField] private float gravity = Physics.gravity.y;
+        [SerializeField] private float lateralGScale = 10f; //Scaling factor for lateral G forces;
+
+        [Header("Banking")] [SerializeField] private float maxBankAngle = 5f;
+        [SerializeField] private float bankSpeed = 2f;
+
+        [Header("References")] [SerializeField]
+        InputReader input;
+
         Rigidbody rb;
-         void Start()
-         {
-             rb = GetComponent<Rigidbody>();
-             input.Enable();
 
-             foreach (AxleInfo axleInfo in axleInfos)
-             {
-                 axleInfo.originalForwardFriction = axleInfo.leftWheel.forwardFriction;
-                 axleInfo.originalSidewaysFriction = axleInfo.leftWheel.sidewaysFriction;
-             }
-             
-         }
+        private float brakeVelocity;
+        private Vector3 kartVelocity;
+        private float driftVelocity;
 
-         private void FixedUpdate()
-         {
-             float verticalInput = AdjustInput(input.Move.y);
-             float horizontalInput = AdjustInput(input.Move.x);
+        private RaycastHit hit;
 
-             float motor = maxMotorTorque * verticalInput;
-             float steering = maxSteeringAngle * horizontalInput;
+        const float thresholdSpeed = 10f;
+        private const float centerOfMassOffset = -0.5f;
+        private Vector3 originalCentorOfMass;
 
-             UpdateAxles(motor, steering);
-         }
-
-         void UpdateAxles(float motor, float steering)
-         {
-             foreach (AxleInfo axleInfo in axleInfos)
-             {
-                 HandleSteering(axleInfo, steering);
-                 HandleMotor(axleInfo, motor);
-                 HandleBreaksAndDrift(axleInfo);
-                 UpdateWheelVisiuals(axleInfo.leftWheel);
-                 UpdateWheelVisiuals(axleInfo.rightWheel);
-             }
-         }
-
-         private void UpdateWheelVisiuals(WheelCollider collider)
-         {
-             if (collider.transform.childCount == 0) return;
-             {
-                 Transform visualWheel = collider.transform.GetChild(0);
-
-                 Vector3 position;
-                 Quaternion rotation;
-                 
-                 collider.GetWorldPose(out position, out rotation);
-
-                 visualWheel.transform.position = position;
-                 visualWheel.transform.rotation = rotation;
-             }
-         }
-
-         void HandleSteering(AxleInfo axleInfo, float steering)
-         {
-             if (axleInfo.steering)
-             {
-                 axleInfo.leftWheel.steerAngle = steering;
-                 axleInfo.rightWheel.steerAngle = steering;
-             }
-         }
-
-         void HandleMotor(AxleInfo axleInfo, float motor)
-         {
-             if (axleInfo.motor)
-             {
-                 axleInfo.leftWheel.motorTorque = motor;
-                 axleInfo.rightWheel.motorTorque = motor;
-             }
-         }
-
-         void HandleBreaksAndDrift(AxleInfo axleInfo)
-         {
-             if (axleInfo.motor)
-             {
-                 if (input.IsBraking)
-                 {
-                     rb.constraints = RigidbodyConstraints.FreezePositionX;
-
-                     float newZ = Mathf.SmoothDamp(rb.velocity.z, 0, ref brakeVelocity, 1f);
-
-                     rb.velocity = rb.velocity.With(z: newZ);
-
-                     axleInfo.leftWheel.brakeTorque = 0;
-                     axleInfo.rightWheel.brakeTorque = 0;
-                 }
-                 else
-                 {
-                     rb.constraints = RigidbodyConstraints.None;
-
-                     axleInfo.leftWheel.brakeTorque = 0;
-                     axleInfo.rightWheel.brakeTorque = 0;
-                 }
+        public bool IsGrounded = true;
+        public Vector3 Velocity => kartVelocity;
+        public float MaxSpeed => maxSpeed;
 
 
-             }
-             
-         }
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            input.Enable();
 
-         float AdjustInput(float input)
-         {
-             return input switch
-             {
-                 >= .7f => 1f,
-                 <= -0.7f => -1f,
-                 _ => input
-             };
-         }
+            rb.centerOfMass = centerOfMass.localPosition;
+            originalCentorOfMass = centerOfMass.localPosition;
+
+            foreach (AxleInfo axleInfo in axleInfos)
+            {
+                axleInfo.originalForwardFriction = axleInfo.leftWheel.forwardFriction;
+                axleInfo.originalSidewaysFriction = axleInfo.leftWheel.sidewaysFriction;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            float verticalInput = AdjustInput(input.Move.y);
+            float horizontalInput = AdjustInput(input.Move.x);
+
+            float motor = maxMotorTorque * verticalInput;
+            float steering = maxSteeringAngle * horizontalInput;
+
+            UpdateAxles(motor, steering);
+            UpdateBanking(horizontalInput);
+
+            kartVelocity = transform.InverseTransformDirection(rb.velocity);
+
+            if (IsGrounded)
+            {
+                HandleGroundedMovement(verticalInput, horizontalInput);
+            }
+            else
+            {
+                HandleAirborneMovement(verticalInput, horizontalInput);
+            }
+        }
+
+        private void UpdateBanking(float horizontalInput)
+        {
+            //Bank the Kart in the opposite direction of the turn
+            float targetBankAngle = horizontalInput * -maxBankAngle;
+            Vector3 currentEuler = transform.localEulerAngles;
+            currentEuler.z = Mathf.LerpAngle(currentEuler.z, targetBankAngle, Time.deltaTime * bankSpeed);
+            transform.localEulerAngles = currentEuler;
+        }
+
+        private void HandleGroundedMovement(float verticalInput, float horizontalInput)
+        {
+            if (Mathf.Abs(verticalInput) > 0.1f || Mathf.Abs(kartVelocity.z) > 1)
+            {
+                float turnMultiplier = Mathf.Clamp01(turnCurve.Evaluate(kartVelocity.magnitude / maxSpeed));
+                rb.AddTorque(Vector3.up *
+                             (horizontalInput * Mathf.Sign(kartVelocity.z) * turnStrength * 100f *
+                              turnMultiplier)); // Turning logic
+            }
+
+            //Acceleration Logic
+            if (!input.IsBraking)
+            {
+                float targetSpeed = verticalInput * maxSpeed;
+                Vector3 forwardWithoutY = transform.forward.With(y: 0).normalized;
+                rb.velocity = Vector3.Lerp(rb.velocity, forwardWithoutY * targetSpeed, Time.deltaTime);
+            }
+            //Downforce - always push the cart down, using lateral GS to scale the force if the Kart is moveing sideways fast and quick
+            float speedFactor = Mathf.Clamp01(rb.velocity.magnitude / maxSpeed);
+            float lateralG = Mathf.Abs(Vector3.Dot(rb.velocity, transform.right));
+            float downForceFactor = Mathf.Max(speedFactor, lateralG / lateralGScale);
+            rb.AddForce(-transform.up * (downForce * rb.mass * downForceFactor));
+            
+            // Shift Center of Mass
+            float speed = rb.velocity.magnitude;
+            Vector3 centerOfMassAdjustment = (speed > thresholdSpeed)
+                ? new Vector3(0f, 0f,
+                    Mathf.Abs(verticalInput) > 0.1f ? Mathf.Sign(verticalInput) * centerOfMassOffset : 0f)
+                : Vector3.zero;
+
+            rb.centerOfMass = originalCentorOfMass + centerOfMassAdjustment;
+        }
+
+        private void HandleAirborneMovement(float verticalInput, float horizontalInput)
+        {
+            rb.velocity =
+                Vector3.Lerp(rb.velocity, rb.velocity + Vector3.down * gravity,
+                    Time.deltaTime * gravity); //apply gravity to the Karth while its airbone.
+        }
+
+        void UpdateAxles(float motor, float steering)
+        {
+            foreach (AxleInfo axleInfo in axleInfos)
+            {
+                HandleSteering(axleInfo, steering);
+                HandleMotor(axleInfo, motor);
+                HandleBreaksAndDrift(axleInfo);
+                UpdateWheelVisiuals(axleInfo.leftWheel);
+                UpdateWheelVisiuals(axleInfo.rightWheel);
+            }
+        }
+
+        private void UpdateWheelVisiuals(WheelCollider collider)
+        {
+            if (collider.transform.childCount == 0) return;
+            {
+                Transform visualWheel = collider.transform.GetChild(0);
+
+                Vector3 position;
+                Quaternion rotation;
+
+                collider.GetWorldPose(out position, out rotation);
+
+                visualWheel.transform.position = position;
+                visualWheel.transform.rotation = rotation;
+            }
+        }
+
+        void HandleSteering(AxleInfo axleInfo, float steering)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+        }
+
+        void HandleMotor(AxleInfo axleInfo, float motor)
+        {
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+
+        void HandleBreaksAndDrift(AxleInfo axleInfo)
+        {
+            if (axleInfo.motor)
+            {
+                if (input.IsBraking)
+                {
+                    rb.constraints = RigidbodyConstraints.FreezePositionX;
+
+                    float newZ = Mathf.SmoothDamp(rb.velocity.z, 0, ref brakeVelocity, 1f);
+
+                    rb.velocity = rb.velocity.With(z: newZ);
+
+                    axleInfo.leftWheel.brakeTorque = 0;
+                    axleInfo.rightWheel.brakeTorque = 0;
+                }
+                else
+                {
+                    rb.constraints = RigidbodyConstraints.None;
+
+                    axleInfo.leftWheel.brakeTorque = 0;
+                    axleInfo.rightWheel.brakeTorque = 0;
+                }
+            }
+        }
+
+        float AdjustInput(float input)
+        {
+            return input switch
+            {
+                >= .7f => 1f,
+                <= -0.7f => -1f,
+                _ => input
+            };
+        }
     }
 }
